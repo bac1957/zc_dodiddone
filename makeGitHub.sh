a451fc04bbbc7984240edb94e6f3dca5a43aafdb
bac1957 <vassilkov@mail.ru>
2024-07-27 21:59:56 +0300

    new file:   makeGitHub.sh

---
+++ makeGitHub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#git init
+#git add README.md
+#git commit -m "first commit"
+#git branch -M main
+#git remote add origin https://bac1957@github.com/bac1957/zc_dodiddone.git
+git push -u origin main
